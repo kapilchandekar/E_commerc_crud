c117e92879b6c97a98b2c853b9a08e73cc649abe
kapilchandekar <kapilchandekar13637@gmail.com>
2024-03-08 16:20:43 +0530

    again changes
    Signed-off-by: kapilchandekar <kapilchandekar13637@gmail.com>

--- src/http/axios/axios_main.js
+++ src/http/axios/axios_main.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
+import { BASE_URL } from '../../helpers/url'
 
 const axiosMain = axios.create({
 	baseURL:
 		process.env.REACT_APP_ENV_STATUS === 'development'
-			? `${process.env.REACT_APP_BASE_URL}`
+			? `${BASE_URL}`
 			: `${process.env.REACT_APP_END_POINT_URL_PROD}`,
 	headers: {
 		'Content-Type': 'application/json',
